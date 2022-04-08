7018c5ebb70d4c84bd81e3a4cc6102e4be5cd068
Lapuskin <mikhail.lapkovsky.2004.@mail.ru>
2022-04-08 19:43:14 +0300

    Excel version

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(303, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Пуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(500, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 112);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(500, 27);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(518, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "открыть";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(518, 110);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "открыть";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(22, 204);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(459, 27);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Sort Program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        class Human
+        {
+            public Human()
+            {
+                fstName = lstName = fathName = date = null;
+            }
+            public string fstName, lstName, fathName, date;
+            public void Init(string fstNameCell, string lstNameCell, string fathNameCell, string dateCell)
+            {
+                fstName = fstNameCell;
+                lstName = lstNameCell;
+                fathName = fathNameCell;
+                date = dateCell;
+            }
+            public bool Comparison(string fullName, string dateTwo)
+            {
+                string[] names = fullName.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (fstName == names[0] && lstName == names[1] && fathName == names[2] && date == dateTwo)
+                    return true;
+                else
+                    return false;
+            } 
+            public void Write(Excel.Application ExcelApp, int i)
+            {
+                Excel.Workbooks ExelWorkbooks = ExcelApp.Workbooks;
+                Excel.Workbook ExelWorkbookRes = ExelWorkbooks[1];
+                Excel.Worksheet ExelSheetRes = (Excel.Worksheet)ExelWorkbookRes.Sheets[1];
+                ExelSheetRes.Cells[i, 2].Value2 = fstName;
+                ExelSheetRes.Cells[i, 3].Value2 = lstName;
+                ExelSheetRes.Cells[i, 4].Value2 = fathName;
+                ExelSheetRes.Cells[i, 5].Value2 = date;
+            }
+        }
+
+        private Excel.Application ExcelApp;
+        private Word.Application WordApp;
+        private Human hum = new Human();
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDialog1.Filter = "Excel files(*.xlsx)|*.xlsx|All files(*.*)|*.*";
+            openFileDialog2.Filter = "Text files(*.doc)|*.doc|Text(*.docx)|*.docx|All files(*.*)|*.*";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateTableRes()
+        {
+            ExcelApp = new Excel.Application();
+            ExcelApp.Visible = true;
+            ExcelApp.SheetsInNewWorkbook = 1;
+            ExcelApp.DisplayAlerts = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreateTableRes();
+            Excel.Worksheet ExcelName = OpenExcel();
+            Word.Table WordName = OpenWorld();
+            ReadExel(hum, ExcelName, WordName);
+            SaveRes();
+            MessageBox.Show("Успех! Ищите файл на рабочем столе.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename1 = openFileDialog2.FileName;
+            textBox2.Text = filename1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = openFileDialog1.FileName;
+            textBox1.Text = filename;
+        }
+        private Excel.Worksheet OpenExcel()
+        {
+            ExcelApp = new Excel.Application();
+            ExcelApp.Visible = true;
+            Excel.Workbook ObjWorkBook = ExcelApp.Workbooks.Open(openFileDialog1.FileName, 0, false, 5, "", "", false, Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
+            Excel.Worksheet ObjSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
+            return ObjSheet;
+        }
+        private void ReadExel(Human hum, Excel.Worksheet ObjSheet, Word.Table ObjWord)
+        {
+            for (int i = 2; i < ObjSheet.UsedRange.Rows.Count + 1; i++)
+            {
+                hum.Init(ObjSheet.Cells[i, 2].Value2, ObjSheet.Cells[i, 3].Value2, ObjSheet.Cells[i, 4].Value2, ObjSheet.Cells[i, 5].Value2);
+                ReadWord(hum, ObjWord);
+            }
+        }
+        private Word.Table OpenWorld()
+        {
+            WordApp = new Word.Application();
+            Object FileName = openFileDialog2.FileName;
+            WordApp.Documents.Open(ref FileName);
+            Word.Document ObjDoc = WordApp.ActiveDocument;
+            Word.Table ObjTable = ObjDoc.Tables[1];
+            return ObjTable;
+        }
+        private void ReadWord(Human hum, Word.Table ObjTable)
+        {
+            int NumberOfMans = 1;
+            for (
+                int i = 2; i < ObjTable.Columns.Count + 1; i++)
+            {
+                if (ObjTable.Cell(i, 1).Range.Text.Trim('\a', '\r', '\n', '\t') == NumberOfMans.ToString())
+                {
+                    if (hum.Comparison(ObjTable.Cell(i, 2).Range.Text.Trim('\a', '\r', '\n', '\t'), ObjTable.Cell(i, 3).Range.Text.Trim('\a', '\r', '\n', '\t')))
+                    {
+                        hum.Write(ExcelApp, Convert.ToInt32(NumberOfMans));
+                    }
+                    Convert.ToInt32(NumberOfMans);
+                    NumberOfMans++;
+                }
+            }
+        }
+
+        private void SaveRes()
+        {
+            Excel.Workbooks ExelWorkbooks = ExcelApp.Workbooks;
+            Excel.Workbook ExelWorkbookRes = ExelWorkbooks[1];
+            ExcelApp.DefaultSaveFormat = Excel.XlFileFormat.xlExcel9795;
+            ExcelApp.DisplayAlerts = true;
+            ExelWorkbookRes.Saved = true;
+            ExelWorkbookRes.Save();
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
